f4674072d019bb08304f6265849279f3449aa0ab
mac_for_hut <1147681296@qq.com>
2019-06-26 00:03:59 +0800

    add solution

---
+++ src/com/leedcode/hash/valid_anagram.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+/**
+ * 1. sort 
+ * 2. hash
+ */
+class Solution {
+    private HashMapp<Characher, Integer> sMap;
+    private HashMapp<Characher, Integer> tMap;
+    
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length())
+            return false;    
+
+        sMap = new HashMap<Characher, Integer>();
+        for (int i = 0; i < s.length(); i++) {
+            if ()
+            sMap.put(s.charAt(i), );
+        }
+
+        return 1;
+    }
+}
