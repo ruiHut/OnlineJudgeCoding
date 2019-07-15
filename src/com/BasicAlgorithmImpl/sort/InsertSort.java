f4c4ae4a84a9cd99c9035601bb35608cda962d61
mac_for_hut <1147681296@qq.com>
2019-07-15 10:20:57 +0800

    feat: add insertSort realized and use binaryInsertSort opt

--- src/com/BasicAlgorithmImpl/sort/InsertSort.java
+++ src/com/BasicAlgorithmImpl/sort/InsertSort.java
@@ -36,4 +36,32 @@ public class InsertSort {
         args[i] = args[j] ^ args[i];
         args[j] = args[j] ^ args[i];
     }
+
+    public int[] insertSort(int[] arr) {
+        if (arr.lenth == 0 || arr.lenth == 1) return arr;
+
+        int len = arr.lenth;
+        for (int i = 1; i < len; i++) {
+                int key = arr[i]; //  保存当前key值
+                int j = i - 1;
+                while (j >= 0 && arr[j] > key) {
+                        arr[j + 1] = arr[j];
+                        j--;
+                }
+                array[j + 1] = key;
+        }
+        return arr;
+    }
+
+    // 优化 使用二分查找法减少比较操作的数目
+    public int[] binaryInsertSort(int arr[]) {
+        if (arr.length == 0 || arr.length == 1) return arr;
+
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int repIndex = binaryFind(arr, 0, i, i / 2); // 二分查找出待替换值
+            replaceSwap(arr, repIndex, i);  // 从该节点开始往后推移
+            return arr;
+        }
+    }
 }
