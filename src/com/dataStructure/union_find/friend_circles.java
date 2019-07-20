7fb6fa573d4e77372f5bbcdbc64ef49f97fbe33a
mac_for_hut <1147681296@qq.com>
2019-07-20 15:53:36 +0800

    feat(com/dataStructure): add friend_circles realized

--- src/com/dataStructure/union_find/friend_circles.java
+++ src/com/dataStructure/union_find/friend_circles.java
@@ -4,3 +4,88 @@
  * 1. 染色 （BFS DFS）
  * 2. 并查集
  */
+
+// FloodFill DFS
+class Solution {
+    static int rows, colums;
+    static int[][] checker;
+    static int[][] distance = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+    public int findCircleNum(int[][] M) {
+        if (M.length==0 || M[0].length==0) return 0;
+        
+        checker = M;
+        rows = checker.length;
+        colums = checker[0].length;
+        int sum = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < colums; j++) {
+                if (checker[i][j] == 1) {
+                  sum++;
+                  floodFillDFS(i, j);
+                }
+            }
+        }
+        return sum;
+    }
+    
+    private void floodFillDFS(int x, int y) {
+        if (!valid(x, y)) return;
+
+        checker[x][y] = 0;
+        for (int[] d : distance) 
+            floodFillDFS(x + d[0], y + d[1]);
+        
+        return;
+    }
+    private boolean valid(int x, int y) {
+        return x < 0 || x >= rows || y < 0 || y >= colums ||  checker[x][y] != 1 ? false : true;
+    }
+}
+
+// use Union_Find
+class Solution {
+    int[][] distance = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+    int[][] checker;
+    int rows, colums;
+    int[] father;
+    public int findCircleNum(int[][] M) {
+        if (M.length == 0 || M[0].length == 0) return 0;
+    
+        checker = M;
+        rows = checker.length;
+        colums = checker[0].length;
+        father = new int[rows * colums];
+        int count = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < colums; j++) {
+                if (checker[i][j] == 1) {
+                    count++;
+                    // init uf
+                    int cur = i * colums + j;
+                    father[cur] = cur;
+                    // floodFill 
+                    for (int[] d : distance) {
+                        // isValid node
+                        int x = i + d[0];
+                        int y = j + d[1];
+                        if (x < 0 || x >= rows || y < 0 || y >= colums || checker[x][y] != 1) continue;
+
+                        if (find(cur) == find(x * colums + y)){
+                            continue;
+                        } else {
+                            count--;
+                            father[find(x * colums + y)] =  find(cur);
+                        }
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    private int find(int node) {
+        if (father[node] == node) return node;
+
+        return find(father[node]);
+    }
+}
