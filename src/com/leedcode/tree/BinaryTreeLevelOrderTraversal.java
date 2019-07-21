bd63cc20c0cb5457ee96d31f9144c514173a7f49
mac_for_hut <1147681296@qq.com>
2019-07-21 20:35:45 +0800

    feat(tree/binarytree): add solution for 102, 103 use DFS

--- src/com/leedcode/tree/BinaryTreeLevelOrderTraversal.java
+++ src/com/leedcode/tree/BinaryTreeLevelOrderTraversal.java
@@ -41,5 +41,30 @@ class Solution {
     }
 }
 
-// 2 iterativeLeveOrder DFS fixme 
+// 2 iterativeLeveOrder DFS
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null)
+            return res;
+        levelOrderHelper(res, root, 0);
+        return res;
+    }
 
+    public void levelOrderHelper(List<List<Integer>> res, TreeNode root, int level) {
+        if (root == null)
+            return;
+        List<Integer> curr;
+        if (level >= res.size()) {
+            curr = new ArrayList<>();
+            curr.add(root.val);
+            res.add(curr);
+        } else {
+            curr = res.get(level); 
+            curr.add(root.val); 
+            //res.add(curr); // No need to add the curr into the res, because the res.get(index) method does not remove the index element
+        }
+        levelOrderHelper(res, root.left, level + 1);
+        levelOrderHelper(res, root.right, level + 1);
+    }
+}
